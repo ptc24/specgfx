2f8c0ee7918d8d2b11de315ec0fdaf043b4b7681
Peter Corbett <peter.corbett@cantab.net>
2020-09-01 21:37:57 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
 ]
 
 autodoc_mock_imports = ["pygame", "numpy"]
+master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
